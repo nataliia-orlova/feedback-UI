dcbad41edc0b776cbbb1519b761685e8763fa638
Nataliia Orlova <nataliia.orlova.dev@gmail.com>
2023-12-16 14:33:52 +0100

    Add feedback list update

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import Header from './components/Header';
+import { v4 as uuidv4 } from 'uuid';
 import FeedbackList from './components/FeedbackList';
 import FeedbackData from './data/FeedbackData';
 import FeedbackStats from './components/FeedbackStats';
@@ -7,6 +8,13 @@ import FeedbackForm from './components/FeedbackForm';
 
 function App() {
     const [feedback, setFeedback] = useState(FeedbackData);
+    const addFeedback = (newFeedback) => {
+        newFeedback.id = uuidv4();
+        // get the current feedback items as an array (copy it) and add new feedback
+        setFeedback([newFeedback, ...feedback]);
+        console.log(newFeedback);
+    };
+
     const deleteFeedback = (id) => {
         if (
             window.confirm(
@@ -20,7 +28,7 @@ function App() {
         <div>
             <Header />
             <div className='container'>
-                <FeedbackForm />
+                <FeedbackForm handleAdd={addFeedback} />
                 <FeedbackStats feedback={feedback} />
                 <FeedbackList
                     feedback={feedback}
--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react';
 import Card from './shared/Card';
 import Button from './shared/Button';
+import RatingSelect from './RatingSelect';
 
-export default function FeedbackForm() {
+export default function FeedbackForm({ handleAdd }) {
     const [text, setText] = useState('');
     const [btnDisabled, setBtnDisabled] = useState(true);
     const [message, setMessage] = useState('');
+    const [rating, setRating] = useState(10);
 
     const handleTextChange = (e) => {
         if (text === '') {
@@ -20,10 +22,24 @@ export default function FeedbackForm() {
         }
         setText(e.target.value);
     };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (text.trim().length > 10) {
+            const newFeedback = {
+                text: text,
+                rating: rating,
+            };
+            // console.log(newFeedback);
+            handleAdd(newFeedback);
+            setText('');
+        }
+    };
     return (
         <Card>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <h2>How would you rate our service?</h2>
+                <RatingSelect select={(rating) => setRating(rating)} />
                 <div className='input-group'>
                     <input
                         onChange={handleTextChange}
@@ -36,7 +52,7 @@ export default function FeedbackForm() {
                         version='primary'
                         isDisabled={btnDisabled}
                     >
-                        TEST
+                        Send
                     </Button>
                 </div>
                 {message && <div className='message'>{message}</div>}
